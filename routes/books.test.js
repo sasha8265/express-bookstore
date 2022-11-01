2cf2c0563f1ae45fe0f53649485690c0d33daed6
Sasha Czerniawski <sashacandoit@gmail.com>
2022-11-01 07:17:44 -0400

    Post Put and delete request tests done

--- routes/books.test.js
+++ routes/books.test.js
@@ -1,5 +1,6 @@
 process.env.NODE_ENV === "test"
 
+const Book = require("../models/book");
 const request = require("supertest");
 const app = require("../app");
 const db = require("../db");
@@ -59,4 +60,95 @@ describe("GET /books/:isbn", () => {
         const res = await request(app).get(`/books/xyz`)
         expect(res.statusCode).toBe(404);
     });
+})
+
+/** POST /books/ - create book from data;
+ * return data about new book: `{book: book}` */
+describe("POST /books", () => {
+    test("Creates a single book and adds to db", async () => {
+        const res = await request(app).post('/books').send({
+            isbn: '9999999999',
+            amazon_url: 'amazon- url.com',
+            author: 'Test Author',
+            language: 'english',
+            pages: 100,
+            publisher: 'Test Publisher',
+            title: 'Test Title',
+            year: 2000
+        });
+        expect(res.statusCode).toBe(201);
+        expect(res.body.book).toHaveProperty("isbn");
+    });
+    test("Prevents book from being added if book data doesn't follow schema", async () => {
+        const res = await request(app).post('/books').send({
+            isbn: 9999999999,
+            amazon_url: 'amazon- url.com',
+            author: 'Test Author',
+            language: 'english',
+            pages: 100,
+            title: 'Test Title',
+            year: 2000
+        });
+        expect(res.statusCode).toBe(400);
+        expect(res.body.error).toEqual({
+                "message": [
+                    "instance requires property \"publisher\"",
+                    "instance.isbn is not of a type(s) string"
+                ], "status": 400
+        })
+    })
+})
+
+/** PUT /books/[isbn] - retrieve book by isbn and update from data;
+ * return data about updated book: `{book: book}` 
+ * Return 404 if book not found
+*/
+describe("PUT /books/:isbn", () => {
+    test("Updates a single book", async () => {
+        const res = await request(app).put(`/books/${testBook.isbn}`).send({
+            amazon_url: 'amazon-url.com',
+            author: 'Test Author',
+            language: 'english',
+            pages: 150,
+            publisher: 'Test Publisher',
+            title: 'Updated Test Book',
+            year: 2000
+        });
+        expect(res.statusCode).toBe(200);
+        expect(res.body.book.title).toBe("Updated Test Book");
+        expect(res.body.book.pages).toBe(150);
+    });
+    test("Prevents updating a book with invalid data", async () => {
+        const res = await request(app).put(`/books/${testBook.isbn}`).send({
+            amazon_url: 'amazon-url.com',
+            author: 'Test Author',
+            language: 'english',
+            pages: '150',
+            publisher: 'Test Publisher',
+            title: 'Updated Test Book',
+            year: 2000
+        });
+        expect(res.statusCode).toBe(400);
+        expect(res.body.error).toEqual({
+            "message": [
+                "instance.pages is not of a type(s) integer"
+            ], "status": 400
+        });
+    });
+    test("Responds with 404 if book isbn cannot be found", async () => {
+        const res = await request(app).get(`/books/xyz`)
+        expect(res.statusCode).toBe(404);
+    });
+})
+
+/** DELETE /books/[isbn] - delete book,
+ *  return `{message: "Book deleted"}` */
+describe("DELETE /books/:isbn", () => {
+    test("Deletes a single book", async () => {
+        const res = await request(app).delete(`/books/${testBook.isbn}`);
+        const allBooks = await request(app).get('/books');
+        expect(res.statusCode).toBe(200);
+        expect(res.body).toEqual({ message: "Book deleted" })
+        expect(allBooks.body.books).toEqual([]);
+    })
 })
